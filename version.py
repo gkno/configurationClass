447344cdb8d00b17b4f61dd1a73e845c3391421e
Alistair Ward <AlistairNWard@gmail.com>
2014-10-24 16:36:57 -0400

    Version 0.152 - October 2014
    1. Added the ability to write values as a comma separated list.

--- version.py
+++ version.py
@@ -4,5 +4,5 @@ Version information for configurationClass, created during installation.
 """
 
 __author__  = 'Alistair Ward'
-__version__ = '0.151'
+__version__ = '0.152'
 __date__    = 'October 2014'
